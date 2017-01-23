5678617bf7d0b95d8f3a2c41064c8a89df869cbb
nassy <stasya.aska@gmail.com>
2017-01-23 15:06:25 -0500

    solved task (easy mode) for lesson 2

---
+++ les_2.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Task-1: List of fruits. Build a program wich show numerated right-aligned list of fruits
+
+def format_list(list):
+    max_len = 2
+    for i in list:
+        if len(i) > max_len:
+            max_len=len(i)
+    for i,x  in enumerate(list):
+        #x = '{:>max_len}'.format(i)
+        #print(x)
+        print('{}.{:>{max_len}}'.format(i+1, x, max_len=max_len+1))
+
+list = ['apple', 'orange', 'sdfsdfdsfsdfsdfsdfsd', 'berry', '', 'er']
+print('Task#1')
+format_list(list)
+
+# Task-2:
+# You have 2 lists. Need to remove all elements of 2nd list from the first list
+def remove_repeated(list1, list2):
+    for i in list2[:]:
+        while i in list1:
+            list1.remove(i)
+    print(list1)
+    return list1
+
+print('Task#2')
+list1 = [2, 14, 10, 'a', 'qa', '', 1, 14]
+list2 = [14, 10, 'qa', '']
+remove_repeated(list1, list2)
+
+# Task-3:
+# You have any list of int numbers. Need to get new list appropriate to requirements:
+# if even then divide it 4, else multiply 2
+
+def change_list(list):
+    x = 0
+    for i in list:
+        if i%2 == 0:
+            i = i/4
+            list[x] = i
+            x += 1
+        else:
+            i = i*2
+            list[x] = i
+            x += 1
+    return list
+
+list3 = [4, 5, 8, 10, 0, 1, 50, 4, 1]
+print('Task#3')
+print(change_list(list3))
