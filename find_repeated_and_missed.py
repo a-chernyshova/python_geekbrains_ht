c3deb92a8d9f69051e84c54a4c60b6db050a9bef
nassy <stasya.aska@gmail.com>
2017-03-31 18:12:46 -0400

    deleted redundunt comments

--- find_repeated_and_missed.py
+++ find_repeated_and_missed.py
@@ -1,39 +1,30 @@
 # -*- coding: utf-8 -*-
+# create sequence of integer numbers with 1 missed number and 1 repeated (froom 1 to n)
 import random
 
-#create sequence of integer numbers with 1 missed number and 1 repeated (froom 1 to n)
+
 def create_list(n):
-    result_list = list(range(1, n))
-    #print(result_list)
+    result_list = list(range(1, n+1))
     missed_number = random.choice(result_list)
     result_list.remove(missed_number)
     repeated_number = random.choice(result_list)
     result_list.insert(result_list.index(repeated_number), repeated_number)
     return result_list
 
-# def find_missed(num_list, n):
-#     missed = [x for x in list(range(1,n)) if x not in num_list]
-#     return missed[0]
-#
-# def find_repeated(num_list):
-#     repeated = [x for x in num_list if num_list.count(x)>1]
-#     return repeated[0]
 
-def find_missed_and_repeated(num_list, n):
+def find_missed_and_repeated(num_list):
     missed = ''
     repeated = ''
-    for i in list(range(1,n)):
+    for i in list(range(1, len(num_list))):
         if i not in num_list:
             missed = i
-        elif num_list.count(i)>1:
+        elif num_list.count(i) > 1:
             repeated = i
-    return missed, repeated
+    return 'Missed: '+str(missed) + ',' + 'Repeated: '+str(repeated)
 
 
 if __name__ == '__main__':
     num_list = create_list(15)
     print(num_list)
-    # can be modifyed if we don't know the last number from 1 to len(list)
-    #print("Missed:",find_missed(num_list, 15))
-    #print("Repeated:", find_repeated(num_list))
-    print(find_missed_and_repeated(num_list, 15))
+    # can be modified if we don't know the last number from 1 to len(list)
+    print(find_missed_and_repeated(num_list))
