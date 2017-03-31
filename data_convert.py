75fd872dff1d60e0df816f6d7c05a041751560dc
nassy <stasya.aska@gmail.com>
2017-03-31 17:46:33 -0400

    solved time convert problem + tests

--- data_convert.py
+++ data_convert.py
@@ -1,20 +1,27 @@
 # -*- coding: utf-8 -*-
 #Given a time in -hour AM/PM format, convert it to military (-hour) time.
-# time = input().strip()
-# if time[-2:] == 'PM':
-#     if time[:2] == '12':
-#         result = time[:-2]
-#     else:
-#         result = str(int(time[:2]) + 12) + time[2:-2]
-# elif time[:2] == '12':
-#     result = '00' + time[2:-2]
-# else:
-#     result = time[0:-2]
-# print(result)
 
-def foo(data):
-    if data < 0:
-        return
-    yield 10
-    return 1
-print(foo(-1))
+
+def time_convert(time):
+    time = time.strip()
+    if int(time[:2]) <= 12 and str(time[-2:]) in ['PM', 'pm', 'am', 'AM']:
+        if time[-2:] == 'PM' or time[-2:] == 'pm':
+            if time[:2] == '12':
+                result = time[:-2]
+            else:
+                result = str(int(time[:2]) + 12) + time[2:-2]
+        elif time[:2] == '12':
+            result = '00' + time[2:-2]
+        else:
+            result = time[0:-2]
+        return result
+    else:
+        return 'Incorrect input: '+time
+
+if __name__ == '__main__':
+    print(time_convert('12:05:59PM'))
+    print(time_convert('03:05:59PM'))
+    print(time_convert('12:05:59AM'))
+    print(time_convert('10:05:59pm'))
+    print(time_convert('15:05:59pm'))
+    print(time_convert('12:05:59'))
