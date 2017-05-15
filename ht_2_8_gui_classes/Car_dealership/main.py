93c3fc60142dfea2ff4489812886ef41b14fc8b4
nassy <stasya.aska@gmail.com>
2017-05-15 13:27:34 -0400

    fixes according codereview

--- ht_2_8_gui_classes/Car_dealership/main.py
+++ ht_2_8_gui_classes/Car_dealership/main.py
@@ -7,86 +7,84 @@ from GUI.Regular import Manager
 from GUI.validator import validator
 from Deal.Dealership import Dealership_object
 
-# CR: move all strig static values to constants
-# CR: move window creation to a separate function
-root = Tk()
-root.title("Authorization")
-root.geometry('250x70+350+275')
-root.resizable(width=False, height=False)
-# CR: log is a bad name
-log = Entry(root, width=20)
-log.grid(row=1, column=2, padx=(1, 1))
-Label(root, text="Login", width=15).grid(row=1, column=1)
-passwd = Entry(root, width=20, show='*')
-passwd.grid(row=2, column=2, padx=(1, 1))
-Label(root, text="Password").grid(row=2, column=1)
+
+TIMESTAMP = datetime.datetime.today().strftime("%d.%m.%Y %H:%M:%S")
+ADMIN_ROLE = 'ADMIN'
+MANAGER_ROLE = 'MANAGER'
+USER_ROLE = 'READER'
+CORRECT_FIELD = 'white'
+
+
+def error_window():
+    err_window = Tk()
+    err_window.title("ERROR")
+    err_window.minsize(250, 50)
+    err_window.geometry('550x70+350+200')
+    out = Text(err_window, font="Arial 12", width=150, height=10)
+    out.grid(row=1, column=1, padx=(1, 1))
+    out.insert("0.0", TIMESTAMP + " ERROR: Вы ввели не верный логин или "
+                                  "пароль,\n либо Вы не зарегистрированны.")
+    err_window.mainloop()
+
+
+def draw_gui(role):
+    root.withdraw()
+    if role == ADMIN_ROLE:
+        interface = Admin()
+    elif role == MANAGER_ROLE:
+        interface = Manager()
+    elif role == USER_ROLE:
+        interface = Reader()
+    interface.gui()
 
 
 def authorization():
-    login = log.get()
+    login = login_field.get()
     password = passwd.get()
-
     if login.isalnum() and password.isalnum():
         text = Dealership_object.authorization(login, password)
-        # отображение интерфейса администратора
-        # CR: rename roles to something meaningful
-        if str(text[0]) == '1':
-            #CR: use strftime for date formatting instead of :19
-            print(str(datetime.datetime.today())[:19] + " INFO: Login & password "
-                                                         "are correct. Welcome for ADMIN")
-
-            # CR: repeatable code should be moved to a function
-            root.withdraw()
-            interface = Admin()
-            interface.gui()
-        # отображение интерфейса обычного пользователя
-        elif str(text[0]) == '0':
-            print(str(datetime.datetime.today())[:19] + " INFO: Login & password "
-                                                        "are correct. Welcome for USER")
-            root.withdraw()
-            interface = Manager()
-            interface.gui()
-        elif str(text[0]) == '2':
-            # CR: don't mix languages
-            print(str(datetime.datetime.today())[:19] + " INFO: Получен доступ "
-                                                        "только на просмотр данных")
-            root.withdraw()
-            interface = Reader()
-            interface.gui()
-        # окно с ошибкой
-        # CR: move error window into separate function and give it self-explainable name
+        if text[0] == ADMIN_ROLE:
+            print(TIMESTAMP + " INFO: Login & password are correct. Welcome for " + ADMIN_ROLE)
+            draw_gui(ADMIN_ROLE)
+        elif text[0] == MANAGER_ROLE:
+            print(TIMESTAMP + " INFO: Login/password are correct. Welcome for " + MANAGER_ROLE)
+            draw_gui(MANAGER_ROLE)
+        elif text[0] == USER_ROLE:
+            print(TIMESTAMP + " INFO: Read only access")
+            draw_gui(USER_ROLE)
         else:
-            rooter = Tk()
-            rooter.title("ERROR")
-            root.minsize(30, 50)
-            out = Text(rooter, bg="white", font="Arial 12", width=50, height=10)
-            out.grid(row=1, column=1, padx=(1, 1))
-            out.insert("0.0", str(datetime.datetime.today())[:19] + " ERROR: Вы ввели не "
-                                                                    "верный логин или пароль, "
-                                                                    "либо Вы не зарегистрированны.")
-            rooter.mainloop()
+            error_window()
     else:
         if not login.isalnum() and password.isalnum():
-            validator(log)
-            passwd["bg"] = 'white'
+            validator(login_field)
+            passwd["bg"] = CORRECT_FIELD
         elif not password.isalnum() and login.isalnum():
             validator(passwd)
-            log['bg'] = 'white'
+            login_field['bg'] = CORRECT_FIELD
         else:
-            validator(log)
+            validator(login_field)
             validator(passwd)
+        error_window()
 
-        # CR: reuse same window creation code for both cases
-        rooter = Tk()
-        rooter.title("ERROR")
-        root.minsize(50, 50)
-        out = Text(rooter, bg="white", font="Arial 12", width=50, height=10)
-        out.grid(row=1, column=1, padx=(1, 1))
-        out.insert("0.0", str(datetime.datetime.today())[:19] + " ERROR: Вы "
-                                                                "ввели не верные данные.")
-        rooter.mainloop()
 
-Button(root, text="LogIn", width=10, height=1, command=authorization).grid(row=3, column=1, padx=(1, 1))
-Button(root, text="Cancel", width=10, height=1, command=root.destroy).grid(row=3, column=2, padx=(1, 1))
-root.mainloop()
+def auth_window():
+    global root
+    root = Tk()
+    root.title("Authorization")
+    root.geometry('250x70+350+275')
+    root.resizable(width=False, height=False)
+    global login_field
+    login_field = Entry(root, width=20)
+    login_field.grid(row=1, column=2, padx=(1, 1))
+    Label(root, text="Login", width=15).grid(row=1, column=1)
+    global passwd
+    passwd = Entry(root, width=20, show='*')
+    passwd.grid(row=2, column=2, padx=(1, 1))
+    Label(root, text="Password").grid(row=2, column=1)
+
+    Button(root, text="LogIn", width=10, height=1, command=authorization).grid(row=3, column=1, padx=(1, 1))
+    Button(root, text="Cancel", width=10, height=1, command=root.destroy).grid(row=3, column=2, padx=(1, 1))
+    root.mainloop()
+
+auth_window()
 Dealership_object.break_connection()
